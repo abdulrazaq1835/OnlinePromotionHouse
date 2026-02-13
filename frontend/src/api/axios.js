c81116b9006648f45112bd8ee32738b5575424a8
Abdulrazaq <abdulrazzak1835@gmail.com>
2026-02-14 02:30:14 +0530

    deployed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,8 @@
-import axios from "axios";
-
+import axios from 'axios'
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api",
-  withCredentials: true, 
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api',
+  withCredentials: true
 });
 
-export default API;
+export default API
