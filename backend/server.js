a34fafe16020b2184d7788b77b7f6f09632b668c
Abdulrazaq <abdulrazzak1835@gmail.com>
2026-02-14 10:43:29 +0530

    main

--- backend/server.js
+++ backend/server.js
@@ -16,24 +16,19 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-// app.use(
-//   cors({
-//     origin: [
-//       'https://online-promotion-house-frontend.vercel.app',  
-//       'http://localhost:5173',  
-//       'http://localhost:3000'
-//     ],
-//     credentials: true,
-//         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-//     allowedHeaders: ['Content-Type', 'Authorization']
-//   })
-// );
 app.use(
   cors({
-    origin: true,
+    origin: [
+      'https://online-promotion-house-frontend.vercel.app',  
+      'http://localhost:5173',  
+      'http://localhost:3000'
+    ],
     credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization']
   })
 );
+
 
 app.get("/", (req, res) => {
   res.send("API is running...");
